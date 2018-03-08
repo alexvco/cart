0abb988cd3dfd993f55412a75e981b123867cb17
alexv7 <alexvarjabedian7@gmail.com>
2018-03-08 17:38:29 -0500

    clean code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def set_shopping_cart
-    if !session[:shopping_cart].present?
-      shopping_cart = ShoppingCart.create
-      session[:shopping_cart] = shopping_cart.id
-      @shopping_cart = ShoppingCart.find(session[:shopping_cart])
-    else
-      @shopping_cart = ShoppingCart.find(session[:shopping_cart])
-    end
+    @shopping_cart = ShoppingCart.find(session[:shopping_cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @shopping_cart = ShoppingCart.create
+    session[:shopping_cart_id] = @shopping_cart.id
   end
-  
+
 end
